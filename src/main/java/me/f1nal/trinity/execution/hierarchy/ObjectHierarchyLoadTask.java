3fb2d88ee8144fe48327ae8103230ea7b1cbbc64
firstfault <mason.smith.1982@tutanota.com>
2024-06-21 05:19:11 -0800

    Fix recursive list add bug :)

--- src/main/java/me/f1nal/trinity/execution/hierarchy/ObjectHierarchyLoadTask.java
+++ src/main/java/me/f1nal/trinity/execution/hierarchy/ObjectHierarchyLoadTask.java
@@ -5,6 +5,7 @@ import me.f1nal.trinity.execution.Execution;
 import me.f1nal.trinity.execution.MethodInput;
 import me.f1nal.trinity.execution.loading.ProgressiveLoadTask;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -21,21 +22,48 @@ public class ObjectHierarchyLoadTask extends ProgressiveLoadTask {
         execution.setClassesLoaded();
 
         final List<ClassInput> classList = execution.getClassList();
-        this.startWork(classList.size() * 2);
+        this.startWork((classList.size() * 2) + 1);
 
         for (ClassInput classInput : classList) {
             classInput.getClassHierarchy().buildHierarchy(execution);
             this.finishedWork();
         }
 
+        List<ClassWithMethod> inheritorMethods = new ArrayList<>();
         for (ClassInput classInput : classList) {
             Set<ClassInput> inheritors = classInput.getClassHierarchy().getExtending();
             for (ClassInput inheritor : inheritors) {
                 for (MethodInput superMethod : classInput.getMethodMap().values()) {
-                    inheritor.addInheritedMethod(superMethod);
+                    if (superMethod.getAccessFlags().isStatic()) {
+                        continue;
+                    }
+
+                    inheritorMethods.add(new ClassWithMethod(inheritor, superMethod));
                 }
             }
             this.finishedWork();
+        }
+        for (ClassWithMethod inheritorMethod : inheritorMethods) {
+            inheritorMethod.getClassInput().addInheritedMethod(inheritorMethod.getMethodInput());
+        }
+        this.finishedWork();
+    }
+
+    private static class ClassWithMethod {
+        private final ClassInput classInput;
+        private final MethodInput methodInput;
+
+        private ClassWithMethod(ClassInput classInput, MethodInput methodInput) {
+            this.classInput = classInput;
+            this.methodInput = methodInput;
+        }
+
+        public ClassInput getClassInput() {
+            return classInput;
+        }
+
+        public MethodInput getMethodInput() {
+            return methodInput;
         }
     }
 }
