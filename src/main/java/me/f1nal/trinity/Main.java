51c30eec880a1d52fe604af4099f064b9a79715c
firstfault <mason.smith.1982@tutanota.com>
2026-07-23 21:32:29 +0300

    chore(trinity): bump version to 0.0.5-alpha1

--- src/main/java/me/f1nal/trinity/Main.java
+++ src/main/java/me/f1nal/trinity/Main.java
@@ -27,7 +27,7 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static final String VERSION = "0.0.4-alpha1";
+    public static final String VERSION = "0.0.5-alpha1";
 
     /**
      * Manages the graphical user interface (GUI) elements and interactions.
